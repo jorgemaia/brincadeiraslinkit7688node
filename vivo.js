03b2bd6da92b261083d037eca7f0d73dbfb19250
Jorge Maia <jorgeasmaia@gmail.com>
2016-09-13 17:49:05 -0300

    Update vivo.js

--- vivo.js
+++ vivo.js
@@ -1,14 +1,14 @@
-const mqtt = require('mqtt')  
-const client = mqtt.connect('ws://test.mosquitto.org:8080/mqtt')
+const mqtt = require('mqtt');
+const client = mqtt.connect('ws://test.mosquitto.org:8080/mqtt');
 
 /**
 * The state of the garage, defaults to closed
 * Possible states : closed, opening, open, closing
 */
 
-var state = 'closed'
+var state = 'closed';
 
 client.on('connect', () => {  
   // Inform controllers that garage is connected
   client.publish('crazytechabs/meetup', 'Estou Vivo')
-})
+});
